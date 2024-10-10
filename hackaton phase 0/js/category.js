659c45ec926381a07c5e1ea89cb27635a97e8c0d
Kenny <kenn667914@gmail.com>
2024-10-10 13:43:26 +0700

    checkpoint

--- hackaton phase 0/js/category.js
+++ hackaton phase 0/js/category.js
@@ -1,11 +1,223 @@
+let userInfo =
+  // object yang pertama
+  {
+    userProfilePicture: "default.png",
+    nama: "John Doe",
+    email: "johndoe@gmail.com",
+    password: "12345",
+    category: [
+      {
+        categoryID: 0,
+        categoryName: "Makanan & Minuman",
+        item: [
+          {
+            // edit di sini
+            itemImage: "/MakananDanMinuman/Indomie.jpeg",
+            itemName: "Indomie",
+            itemPrice: 4000,
+            modalPrice: 3000,
+            quantity: 40,
+          },
+          {
+            itemImage: "/MakananDanMinuman/PocariSweat.jpeg",
+            itemName: "PocariSweat",
+            itemPrice: 5000,
+            modalPrice: 3500,
+            quantity: 24,
+          },
+          {
+            itemImage: "/MakananDanMinuman/PopMie.jpg",
+            itemName: "PopMie",
+            itemPrice: 6000,
+            modalPrice: 4000,
+            quantity: 24,
+          },
+          {
+            itemImage: "/MakananDanMinuman/SilverQueen.jpg",
+            itemName: "SilverQueen",
+            itemPrice: 16000,
+            modalPrice: 13500,
+            quantity: 24,
+          },
+          {
+            itemImage: "/MakananDanMinuman/SlaiOlai.jpeg",
+            itemName: "SlaiOlai",
+            itemPrice: 6000,
+            modalPrice: 4000,
+            quantity: 24,
+          },
+        ],
+      },
+      {
+        categoryID: 1,
+        categoryName: "Peralatan Rumah Tangga",
+        item: [
+          {
+            itemImage: "/PeralatanRumahTangga/MamaLemon.jpg",
+            itemName: "MamaLemon",
+            itemPrice: 20000,
+            modalPrice: 17500,
+            quantity: 40,
+          },
+          {
+            itemImage: "/PeralatanRumahTangga/Rinso.jpg",
+            itemName: "Rinso",
+            itemPrice: 36000,
+            modalPrice: 34000,
+            quantity: 24,
+          },
+          {
+            itemImage: "/PeralatanRumahTangga/SapuLidi.jpg",
+            itemName: "SapuLidi",
+            itemPrice: 18000,
+            modalPrice: 15000,
+            quantity: 40,
+          },
+          {
+            itemImage: "/PeralatanRumahTangga/Sunlight.jpeg",
+            itemName: "Sunlight",
+            itemPrice: 24000,
+            modalPrice: 22000,
+            quantity: 40,
+          },
+          {
+            itemImage: "/PeralatanRumahTangga/SuperPel.jpg",
+            itemName: "SuperPel",
+            itemPrice: 18000,
+            modalPrice: 16000,
+            quantity: 40,
+          },
+        ],
+      },
+      {
+        categoryID: 2,
+        categoryName: "Kesehatan & Kecantikan",
+        item: [
+          {
+            itemImage: "/KesehatanDanKecantikan/Biore.jpg",
+            itemName: "Biore",
+            itemPrice: 18000,
+            modalPrice: 16000,
+            quantity: 40,
+          },
+          {
+            itemImage: "/KesehatanDanKecantikan/Garnier.jpg",
+            itemName: "Garnier",
+            itemPrice: 25000,
+            modalPrice: 23500,
+            quantity: 24,
+          },
+          {
+            itemImage: "/KesehatanDanKecantikan/Kahf.jpg",
+            itemName: "Kahf",
+            itemPrice: 24000,
+            modalPrice: 23000,
+            quantity: 40,
+          },
+          {
+            itemImage: "/KesehatanDanKecantikan/Kelly.jpg",
+            itemName: "Kelly",
+            itemPrice: 25000,
+            modalPrice: 23500,
+            quantity: 40,
+          },
+          {
+            itemImage: "/KesehatanDanKecantikan/Zinc.jpg",
+            itemName: "Zinc",
+            itemPrice: 18000,
+            modalPrice: 16000,
+            quantity: 40,
+          },
+        ],
+      },
+    ],
+    order: [
+      {
+        item: [
+          {
+            itemName: "Indomie",
+            jumlahItem: 7,
+          },
+          {
+            itemName: "MamaLemon",
+            jumlahItem: 3,
+          },
+        ],
+      },
+    ],
+  };
+
 // create category
-function addCategory() {}
+function addCategory(userInfo, namaKategori) {
+  let category = userInfo["category"];
+  let kategoriSudahAda = false;
+  for (let i = 0; i < category.length; i++) {
+    if (namaKategori === category[i]["categoryName"]) {
+      kategoriSudahAda = true;
+    }
+  }
+  if (kategoriSudahAda) {
+    console.log("kategori sudah ada");
+  } else {
+    let newCategory = {
+      categoryID: category.length,
+      categoryName: namaKategori,
+      item: [],
+    };
+    userInfo["category"].push(newCategory);
+  }
+}
+//TEST CASE
+// console.log(addCategory(userInfo, "Kenny"));
+// console.log(userInfo);
 
 // read category
-function getCategory() {}
+function getCategory(userInfo, namaKategori) {
+  let category = userInfo["category"];
+  let categoryFound = false;
+  for (let i = 0; i < category.length; i++) {
+    if (category[i]["categoryName"] === namaKategori) {
+      console.log(category[i]["item"]);
+      categoryFound = true;
+    }
+  }
+  if (categoryFound === false) {
+    console.log("Kategori Tidak Ditemukan");
+  }
+}
+//TEST CASE
+// console.log(getCategory(userInfo, "Kesehatan & Kecantikan"));
+// console.log(userInfo);
 
 // updatecategory
-function updateCategory() {}
+function updateCategory(userInfo, namaKategori, namaItem, itemValue) {
+  let category = userInfo["category"];
+  let categoryFound = false;
+  for (let i = 0; i < category.length; i++) {
+    if (category[i]["categoryName"] === namaKategori) {
+      // MASUKKAN FUNGSI UPDATE ITEM DISINI
+      categoryFound = true;
+    }
+  }
+  if (categoryFound === false) {
+    console.log("Kategori Tidak Ditemukan");
+  }
+}
+//TEST CASE
+// console.log(updateCategory(userInfo, "Kesehatan & Kecantikan", namaItem, itemValue ));
+// console.log(userInfo);
 
 // delete category
-function deleteCategory() {}
+function deleteCategory(userInfo, namaKategori) {
+  let newCategory = [];
+  let category = userInfo["category"];
+  for (let i = 0; i < category.length; i++) {
+    if (category[i]["categoryName"] !== namaKategori) {
+      newCategory.push(category[i]);
+    }
+  }
+  userInfo["category"] = newCategory;
+}
+//TEST CASE
+// console.log(deleteCategory(userInfo, "Kesehatan & Kecantikan"));
+// console.log(userInfo);
--- hackaton phase 0/js/item.js
+++ hackaton phase 0/js/item.js
@@ -1,19 +1,81 @@
+let categoryItem = {
+  categoryID: 0,
+  categoryName: "Makanan & Minuman",
+  item: [
+    {
+      // edit di sini
+      itemImage: "/MakananDanMinuman/Indomie.jpeg",
+      itemName: "Indomie",
+      itemPrice: 4000,
+      modalPrice: 3000,
+      quantity: 40,
+    },
+    {
+      itemImage: "/MakananDanMinuman/PocariSweat.jpeg",
+      itemName: "PocariSweat",
+      itemPrice: 5000,
+      modalPrice: 3500,
+      quantity: 24,
+    },
+    {
+      itemImage: "/MakananDanMinuman/PopMie.jpg",
+      itemName: "PopMie",
+      itemPrice: 6000,
+      modalPrice: 4000,
+      quantity: 24,
+    },
+    {
+      itemImage: "/MakananDanMinuman/SilverQueen.jpg",
+      itemName: "SilverQueen",
+      itemPrice: 16000,
+      modalPrice: 13500,
+      quantity: 24,
+    },
+    {
+      itemImage: "/MakananDanMinuman/SlaiOlai.jpeg",
+      itemName: "SlaiOlai",
+      itemPrice: 6000,
+      modalPrice: 4000,
+      quantity: 24,
+    },
+  ],
+};
+
 // create item
-function addItem() {
-    
+function addItem(
+  categoryItem,
+  namaItem,
+  gambarItem,
+  hargaItem,
+  modalItem,
+  quantityItem
+) {
+  let item = categoryItem["item"];
+  let itemSudahAda = false;
+  for (let i = 0; i < item.length; i++) {
+    if (namaItem === item[i]["itemName"]) {
+      itemSudahAda = true;
+    }
+  }
+  if (itemSudahAda) {
+    console.log("kategori sudah ada");
+  } else {
+    let newItem = {
+      itemImage: "/MakananDanMinuman/Indomie.jpeg",
+      itemName: "Indomie",
+      itemPrice: 4000,
+      modalPrice: 3000,
+      quantity: 40,
+    };
+    categoryItem.push(newItem);
+  }
 }
 
 // read Item
-function getItem() {
-    
-}
+function getItem() {}
 
 // updateItem
-function updateItem() {
-    
-}
+function updateItem() {}
 
 // delete item
-function deleteItem() {
-    
-}
+function deleteItem() {}
